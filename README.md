b48aa93c916bd9c4e7f259cdb70c3683b3964ea9
Dhruva Karkada <dkarkada@gmail.com>
2024-09-26 14:04:21 -0700

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mupify
+
+Important notes:
+ * In your architecture, use ReLU layers (i.e., use nn.ReLU() and not torch.functional.relu)
--- mupify.py
+++ mupify.py
@@ -68,7 +68,7 @@ def get_param(layer_type, param, layer_din, gamma):
     return g, lr, scale
 
 
-def mupify(model, optimizer, readin, readout, width, param='mup', gamma=None):
+def mupify(model, optimizer, readin, readout, gamma, param='mup'):
     """
     Reinitializes a model/optimizer in-place to a chosen parameterization. Function does not return.
     
@@ -77,23 +77,21 @@ def mupify(model, optimizer, readin, readout, width, param='mup', gamma=None):
         optimizer (optim.SGD): SGD optimizer. (Other optimizers not tested and probably don't work.)
         readin (str or nn.Module): The name of (or a pointer to) the readin layer
         readout (str or nn.Module): The name of (or a pointer to) the readout layer
-        width (int): Model width. All hidden layers should have this width. For CNNs, width=channel_dim
+        gamma (float): Sets the laziness/activity of the net. gamma=1 is lazy, gamma=sqrt(width) is active.
         param (str): One of ["stp", "mup", "ntp"]. Selects one of (behaviorally-equivalent) parameterization schemes.
-        gamma (float or None): Sets the activity (richness/laziness) of the net. None defaults to active training. gamma=1 is lazy.
     """
-    
-    if gamma is None:
-        gamma = np.sqrt(width)
     for k, v in model.named_modules():
+        if type(v) == nn.ReLU:
+            set_multiplier(v, np.sqrt(2))
         if not hasattr(v, 'weight'):
             continue
         assert type(v) in [nn.Linear, nn.Conv2d], f"Can't handle module {k} of type {v}"
         
         if type(v) == nn.Linear:
-            chan_out, chan_in = v.weight.shape
+            _, chan_in = v.weight.shape
             wt_d_in = chan_in
         elif type(v) == nn.Conv2d:
-            chan_out, chan_in, kh, kw = v.weight.shape
+            _, chan_in, kh, kw = v.weight.shape
             wt_d_in = chan_in * kh * kw
             
         layer_type = "hidden"
@@ -101,12 +99,8 @@ def mupify(model, optimizer, readin, readout, width, param='mup', gamma=None):
             layer_type = "readin"
         if k==readout or v==readout:
             layer_type = "readout"
-        if chan_out != width and layer_type != "readout":
-            print(f"Warning: d_out of {k} should be {width}, but is {chan_out}.")
-        if chan_in != width and layer_type != "readin":
-            print(f"Warning: d_in of {k} should be {width}, but is {chan_in}.")
 
         g, lr, scale = get_param(layer_type, param, wt_d_in, gamma)
         set_multiplier(v, g)
-        set_init_scale(v, scale*np.sqrt(2))
+        set_init_scale(v, scale)
         set_lr(optimizer, v, lr)
